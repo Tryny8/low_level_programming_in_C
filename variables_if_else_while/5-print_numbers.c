e214ec2a75e9141e408ecefa24d2633f67ca9c95
Tryny8 <vitry.pro.a@gmail.com>
2023-06-19 09:26:40 -0700

    Exo 5: Affichage des chiffres

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Affiche l'alphabet via un putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
