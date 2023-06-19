abd78903ad102a8f0f8e9004e161e1175ccd369a
Tryny8 <vitry.pro.a@gmail.com>
2023-06-19 09:10:48 -0700

    Exo 4: Alphabet sans e et q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Affiche l'alphabet via un putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		if (a != 101 || a != 113)
+			putchar(a);
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
