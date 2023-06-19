8b52607838143f0aa30b08f3f534a713d13f5aa3
Tryny8 <vitry.pro.a@gmail.com>
2023-06-19 09:31:50 -0700

    Exo 7: Affichage alphabet inversé

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Affiche l'alphabet via un putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 122; a <= 97; a--)
+	{
+		putchar(a);
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
