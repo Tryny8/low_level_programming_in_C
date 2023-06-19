8ed552c7d8442af27c12377167ea92fd3153901a
Tryny8 <vitry.pro.a@gmail.com>
2023-06-19 08:30:32 -0700

    Exo 2: Affichage alphabet suivant ASCII

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Affiche l'alphabet via un putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a == 122; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
