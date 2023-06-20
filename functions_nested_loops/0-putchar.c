eead655cb1a4dedc9a133e5291a8b6be6b6a454c
Tryny8 <vitry.pro.a@gmail.com>
2023-06-20 07:56:03 -0700

    Exo 0: Utilisation de la proto-fonction _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+#include <main.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Affiche _putchar avec fontion _putchar
+ *
+ * Description function: _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: Always 0 (Success)
+ */
+int _putchar(char c)
+
+int main(void)
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+	return (0);
+}
