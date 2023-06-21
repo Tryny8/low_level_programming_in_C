c80eaddc0ad7a8b8aa5c309214bdf095fa376dfb
Tryny8 <vitry.pro.a@gmail.com>
2023-06-21 01:54:08 -0700

    Exo 2: print x10 alphabet - test avec 1-alphabet.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - writes the alphabet x10 to _putchar
+ *
+ * Return: On success 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int c;
+
+	for (c = 0; c <= 10; c++)
+	{
+		print_alphabet();
+	}
+	_putchar(10);
+}
