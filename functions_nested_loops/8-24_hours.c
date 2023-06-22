ef8a649a1f8d0501950aafc212e0a7d332e90af0
Tryny8 <vitry.pro.a@gmail.com>
2023-06-22 08:27:28 -0700

    error: boucle for avec ; et non ,

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * jack_bauer  - affiche 24h d'affilé
+ *
+ * Return: On success 0.
+ */
+
+void jack_bauer(void)
+{
+	int heure;
+	int minute;
+
+	for (heure = 0; heure <= 24; heure++)
+	{
+		for (minute = 0; minute <= 60; minute++)
+		{
+			if (heure != 24)
+			{
+				if (minute != 60)
+				{
+					_putchar(heure);
+					_putchar(minute);
+					_putchar('\n');
+				}
+				else
+				{
+					minute = 0;
+				}
+			}
+			else
+			{
+				return;
+			}
+		}
+	}
+}
