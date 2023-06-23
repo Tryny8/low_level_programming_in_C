480124e3b389f2c87fbf95263d6be339574f4356
Tryny8 <vitry.pro.a@gmail.com>
2023-06-23 06:29:13 -0700

    error: une seule table + style

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -16,8 +16,9 @@ void times_table(void)
 		for (m = 0; m <= 9; m++)
 		{
 			resultat = (m * t);
-			printf("%02d", resultat);
-			t = t + 1;
+			printf("%02d, ", resultat);
 		}
+		t = t + 1;
+		_putchar('\n');
 	}
 }
