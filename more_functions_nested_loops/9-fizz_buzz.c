22ba6cb2d0bb6c7efc9375fb6bcbac843eae1561
Tryny8 <vitry.pro.a@gmail.com>
2023-06-25 16:14:19 -0700

    Exo 9: FizzBuzz - multiple de 3 et/ou 5

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - write Write a program that prints the numbers from 1 to 100,
+ * followed by a new line. But for multiples of three print Fizz instead of
+ * the number and for the multiples of five print Buzz. For numbers which
+ * are multiples of both three and five print FizzBuzz.
+ *
+ * Return: On success 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (((n % 3) == 0) && ((n % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		printf("\n");
+	}
+	return (0);
+}
