037689c997ab2056f57478a33db97b43babd4be5
Tryny8 <vitry.pro.a@gmail.com>
2023-06-25 12:36:58 -0700

    Exo 2: multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - affiche la multiplication de deux nombre
+ * @a: entier a
+ * @b: entier b
+ *
+ * Return: On success 0.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
