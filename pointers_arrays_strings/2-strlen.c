ac80da1989f65b92d5cfaf77989422b50d91b176
Tryny8 <vitry.pro.a@gmail.com>
2023-06-27 06:47:46 -0700

    Exo 2: taille d une chaine de caractère

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,23 +1,12 @@
 #include "main.h"
 /**
- * _isupper - checks for uppercase character
- * @c: The charater c to stdoud
+ * _strlen - retour le longueur de la string
+ * @s: chaine de caractère
  *
  * Return: On success 0.
  */
 
-int _isupper(int c)
+int _strlen(char *s)
 {
-	int uppercase;
-
-	uppercase = c;
-
-	if ((uppercase >= 65) && (uppercase <= 90))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (sizeof(s));
 }
