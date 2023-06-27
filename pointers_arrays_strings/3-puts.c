51dc5a46c34352462348b9d0e9eccebafda3975c
Tryny8 <vitry.pro.a@gmail.com>
2023-06-27 14:17:01 -0700

    Exo 3: affiche la chaine de caractère

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,23 +1,18 @@
 #include "main.h"
 /**
- * _isupper - checks for uppercase character
- * @c: The charater c to stdoud
+ * _puts - imprime la chaine de caractère
+ * @str: chaine de caractère
  *
  * Return: On success 0.
  */
 
-int _isupper(int c)
+void _puts(char *str)
 {
-	int uppercase;
+	int nombreDeCaracteres = 0;
+	char caractereActuel = 0;
 
-	uppercase = c;
-
-	if ((uppercase >= 65) && (uppercase <= 90))
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	do {
+		caractereActuel = str[nombreDeCaracteres];
+		_putchar(caractereActuel);
+	} while (caractereActuel != '\0');
 }
