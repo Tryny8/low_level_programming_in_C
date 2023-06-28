7dd461e3cbd0a15d4f9a964e94128c60ffbc7690
Tryny8 <vitry.pro.a@gmail.com>
2023-06-28 06:55:23 -0700

    Exo 4: changement while et variable

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,18 +9,15 @@
 void print_rev(char *s)
 {
 	int len = 0;
-	char char_current = 0;
-	char caractereActuel = 0;
 
-	do {
-		char_current = s[len];
+	while (s[len] != '\0')
+	{
 		len++;
-	} while (char_current != '\0');
-
-	do {
-		caractereActuel = s[len];
+	}
+	while (len >= 0)
+	{
+		_putchar(s[len]);
 		len--;
-		_putchar(caractereActuel);
-	} while (caractereActuel >= 0);
+	}
 	_putchar(10);
 }
