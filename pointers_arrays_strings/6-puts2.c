0696bd8cab657e983d6f19ff159fcae364743750
Tryny8 <vitry.pro.a@gmail.com>
2023-06-28 15:09:01 -0700

    Exo 6: var counter miss

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,19 +9,20 @@
 void puts2(char *str)
 {
 	int len = 0;
+	int counter = 0;
 
 	while (str[len] != '\0')
 	{
-		if ((str[len] % 2) == 0)
+		if ((counter % 2) == 0)
 		{
 			_putchar(str[len]);
 			len++;
 		}
 		else
 		{
-			_putchar(61);
 			len++;
 		}
+		counter++;
 	}
 	_putchar(10);
 }
