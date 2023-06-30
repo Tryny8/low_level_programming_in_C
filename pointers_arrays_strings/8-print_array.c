304f206abd9c8e085b583aa5213d88678b70f6e4
Tryny8 <vitry.pro.a@gmail.com>
2023-06-30 14:42:27 -0700

    Exo 8: cas > à 256 jamais valider

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -16,16 +16,16 @@ void print_array(int *a, int n)
 		if (n > 256)
 		{
 			n = n - 1;
-			for (i = 0; i < n; i++)
-			{
-				printf("%d,", a[i]);
-				if (i != n)
-				{
-					printf(" ");
-				}
-			}
-			printf("\n");
 		}
+		for (i = 0; i < n; i++)
+		{
+			printf("%d,", a[i]);
+			if (i != n)
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
 	}
 	else
 	{
