6ae1c0ee35368ef07a6fb283303c090d536b45f7
Tryny8 <vitry.pro.a@gmail.com>
2023-07-03 07:46:04 -0700

    Exo 9: correction

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - reeset la valeur d'une varaible via une pointeur
+ * @n: pointeur n
+ *
+ * Return: On success 0.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
