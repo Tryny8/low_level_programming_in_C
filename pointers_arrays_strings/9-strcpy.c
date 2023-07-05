20b2b6503cc5b4ef91976f2aae46e5e676688e74
Tryny8 <vitry.pro.a@gmail.com>
2023-07-05 07:33:07 -0700

    Exo 9: pointer - correction src inverse dest

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,13 +12,14 @@ char *_strcpy(char *dest, char *src)
 	int len = 0;
 	int i;
 
-	while (dest[len] != '\0')
+	while (src[len] != '\0')
 	{
 		len++;
 	}
 
 	for (i = 0; i <= len; i++)
 	{
-		src[i] = dest[i];
+		dest[i] = src[i];
 	}
+	return (dest);
 }
