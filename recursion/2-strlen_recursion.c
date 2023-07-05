9596b2638c7376c5b78334ee17113e8c4ddc8470
Tryny8 <vitry.pro.a@gmail.com>
2023-07-05 05:34:58 -0700

    Exo 2: error - return doit traiter tous les cas

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - return la longueur de la chaine
+ * @s: chaine de caractère
+ *
+ * Return: On success 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return ((_strlen_recursion(s + 1)) + 1);
+	}
+	return 0;
+}
