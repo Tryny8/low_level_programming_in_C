2f68ac643f4496e33bfd33eecb75c92c4c2f6be6
Tryny8 <vitry.pro.a@gmail.com>
2023-07-05 06:12:39 -0700

    Exo 4: Recursion - x exposant y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - calcul le factoriel d'un nombre
+ * @x: valeur
+ * @y: power
+ *
+ * Return: On success 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y >= 0)
+	{
+		return (_pow_recursion(x, y - 1) * x);
+	}
+	else
+	{
+		return (-1);
+	}
+}
