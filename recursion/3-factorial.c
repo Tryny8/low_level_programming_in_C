a150c1f59516b870758028c3d6127a2bfa09fba6
Tryny8 <vitry.pro.a@gmail.com>
2023-07-05 05:51:59 -0700

    Exo 3: Récursive - calcul factoriel

---
+++ recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * factorial - calcul le factoriel d'un nombre
+ * @n: nombre
+ *
+ * Return: On success 0.
+ */
+
+int factorial(int n)
+{
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
